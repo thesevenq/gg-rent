85e01c998033197e477f295dd4b6d99775b0dc38
thesevenq <59538879+thesevenq@users.noreply.github.com>
2023-04-10 03:26:08 +0200

    (refactor): added automatic implementation

--- client/renta_cl.lua
+++ client/renta_cl.lua
@@ -1,85 +1,76 @@
 ESX = exports['es_extended']:getSharedObject()
 
-
-function notifikacija(msg)
-    ESX.ShowNotification(msg)
-end
-
 CreateThread(function()
     for k, v in pairs(Config.Renta) do 
-        RequestModel(GetHashKey(Config.Ped))
-        while not HasModelLoaded(GetHashKey(Config.Ped)) do
-        Wait(1)
-        end
-        pedrenta = CreatePed(4, Config.Ped, v.coords, v.heading, false, true)
-        FreezeEntityPosition(pedrenta, true) 
-        SetEntityInvincible(pedrenta, true)
-        SetBlockingOfNonTemporaryEvents(pedrenta, true)
-        exports.qtarget:AddBoxZone(v.imezone, v.coords, 0.85, 0.65, {
-            name=v.imezone,
-            heading=0,
-            debugPoly=Config.Debug,
-            minZ=v.coords.z -1,
-            maxZ=v.coords.z +2,
-            }, {
-                options = {
-                    {   
-                        action = function()
-                        local input = lib.inputDialog("Meni rente vozila", {
-                            { type = 'select', label = "Izaberite vozilo", options = {
-                            { value = 'option1', label = Config.Vozila["vozilo1"].spawnkod },
-                            { value = 'option2', label = Config.Vozila["vozilo2"].spawnkod },
-                            { value = 'option3', label = Config.Vozila["vozilo3"].spawnkod },
-                            }},
-                        })
-                        local opcija = input[1]
-                          if opcija == 'option1' then 
-                        
-                            ESX.TriggerServerCallback('gg-renta:proveri_novac', function(data)
-                                if data then 
-                                ESX.Game.SpawnVehicle(Config.Vozila["vozilo1"].spawnkod, GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId()), function(vozilo1) 
-                                    TaskWarpPedIntoVehicle(PlayerPedId(), vozilo1, -1)
-                                 notifikacija("Uspesno ste iznajmili vozilo " ..Config.Vozila["vozilo1"].spawnkod)
-                                end)
-                            end
-                            end, Config.Vozila["vozilo1"].cena)
-                    
-                            elseif opcija == 'option2' then 
-                                
-                            ESX.TriggerServerCallback('gg-renta:proveri_novac', function(data)
-                                if data then 
-                                ESX.Game.SpawnVehicle(Config.Vozila["vozilo2"].spawnkod, GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId()), function(vozilo2) 
-                                      TaskWarpPedIntoVehicle(PlayerPedId(), vozilo2, -1)
-                                   notifikacija("Uspesno ste iznajmili vozilo " ..Config.Vozila["vozilo2"].spawnkod)
-                                end)
-                            end
-                            end, Config.Vozila["vozilo2"].cena)
-                            elseif opcija == 'option3' then 
-                            ESX.TriggerServerCallback('gg-renta:proveri_novac', function(data)
-                                if data then 
-                                ESX.Game.SpawnVehicle(Config.Vozila["vozilo3"].spawnkod, GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId()), function(vozilo3) 
-                                      TaskWarpPedIntoVehicle(PlayerPedId(), vozilo3, -1)
-                                   notifikacija("Uspesno ste iznajmili vozilo " ..Config.Vozila["vozilo3"].spawnkod)
-                               end)
-                            end
-                            end, Config.Vozila["vozilo3"].cena)
-                             end
-                        end,
-                        label = "Iznajmi vozilo",
-                    },
+        local rentPed = napraviPeda(v)
+        exports.qtarget:AddTargetEntity(rentPed, {
+            options = {
+                {
+                    icon = "fas fa-car",
+                    label = 'Iznajmi Vozilo',
+                    action = function()
+                        otvoriRentu()
+                    end,
                 },
-                distance = 4.5
-        })  
-        local blip = AddBlipForCoord(v.coords)
+            },
+            distance = 3.0
+        })
 
-        SetBlipSprite (blip, 523)
-        SetBlipDisplay(blip, 4)
-        SetBlipScale  (blip, 0.8)
-        SetBlipColour (blip, 1)
-        SetBlipAsShortRange(blip, true)
-    
-        BeginTextCommandSetBlipName('STRING')
-        AddTextComponentSubstringPlayerName('Renta vozila')
-        EndTextCommandSetBlipName(blip)
+        napraviBlip(v)
     end
 end)
+
+function otvoriRentu()
+    local opcije = {}
+
+    for k,v in pairs(Config.RentVozila) do
+        table.insert(opcije, {
+            value = json.encode(v), 
+            label = v.label
+        })
+    end
+
+    local input = lib.inputDialog("Meni rente vozila", {
+        { type = 'select', label = "Izaberite vozilo", options = opcije}
+    })
+
+    local odabranaOpcija = input[1]
+    local podaciVozila = json.decode(odabranaOpcija)
+
+    ESX.TriggerServerCallback('gg-renta:proveri_novac', function(imaDovoljno)
+        if imaDovoljno then 
+            ESX.Game.SpawnVehicle(podaciVozila.spawnKod, GetEntityCoords(PlayerPedId()), GetEntityHeading(PlayerPedId()), function(vozilo) 
+                TaskWarpPedIntoVehicle(PlayerPedId(), vozilo, -1)
+                ESX.ShowNotification("Uspesno ste iznajmili vozilo " .. podaciVozila.label)
+            end)
+        end
+    end, podaciVozila.cena)
+end
+
+function napraviPeda(renta)
+    RequestModel(GetHashKey(Config.Ped))
+    while not HasModelLoaded(GetHashKey(Config.Ped)) do
+        Wait(1)
+    end
+
+    pedrenta = CreatePed(4, Config.Ped, renta.coords, renta.heading, false, true)
+    FreezeEntityPosition(pedrenta, true) 
+    SetEntityInvincible(pedrenta, true)
+    SetBlockingOfNonTemporaryEvents(pedrenta, true)
+
+    return pedrenta
+end
+
+function napraviBlip(renta)
+    local blip = AddBlipForCoord(renta.coords)
+
+    SetBlipSprite (blip, 523)
+    SetBlipDisplay(blip, 4)
+    SetBlipScale  (blip, 0.8)
+    SetBlipColour (blip, 1)
+    SetBlipAsShortRange(blip, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentSubstringPlayerName('Renta Vozila')
+    EndTextCommandSetBlipName(blip)
+end
